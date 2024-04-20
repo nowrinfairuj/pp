ec6bb5ed9fce41769458cb5640a6168452ba8287
nowrinfairuj <154911511+nowrinfairuj@users.noreply.github.com>
2024-04-20 19:46:49 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//lightbox
+ lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true
+    })
+
+//counterup
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+//mixitup
+var mixer = mixitup('#img', {
+   
+    animation: {
+        duration: 300
+    }
+});
